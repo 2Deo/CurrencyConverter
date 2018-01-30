7cd4d10e93586cbf5c93b39e44ea360cfd5d9c97
2Deo <ziemowit.holda@gmail.com>
2018-01-30 12:31:47 +0100

    Second commit

--- app/index.js
+++ app/index.js
@@ -1,22 +1,4 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Home from './screens/Home'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Currency Converter from app/index.js</Text>
-
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default <Home />
